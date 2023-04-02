92d74b4119c81718f54ac3dcac5a490b13a64e82
Sagar <dev.sagartmg@gmail.com>
2023-04-02 15:56:52 +0545

    variables

---
+++ index.js
@@ -0,0 +1,87 @@
+
+console.log("Hello World!")
+
+/* comments
+    System.out.println("Hello Wrld");
+    print()
+*/
+
+/* css variable --primary-color: */
+
+/* variable 
+    box / contianer which can store some values 
+*/
+
+course_name = 'MERN' // text 
+age = 20 // number 
+paid = true  // Boolean - true and false 
+price = 100.10
+
+/* Data Types 
+    1.  String  - text 
+    2. Number 
+        integer // whole number
+        float // decimal 
+    3. Boolean    
+        true
+        false 
+*/
+
+console.log(course_name)
+console.log("age", age)
+console.log("paid", paid)
+console.log("price", price)
+
+/* Naming convention
+    snake_case
+    camelCase
+*/
+
+
+product_1 = "apple"
+
+
+
+
+var brand = "Apple"
+let color = "Red"
+const PI = 3.14
+
+console.log(brand, color, PI);
+
+/* ES - ecmascript */
+/* 2015 - es6  */
+
+
+/* declaratuion and initialziation  */
+
+var time; // declaring time variable 
+time = "3:45PM" // initializing the time variable 
+
+
+let dimension;
+dimension = "3D"
+
+
+var time = "3:46PM"
+
+console.log("time", time);
+
+dimension = "4d"
+dimension = "5d"
+dimension = "6d"
+
+console.log("dimension", dimension);
+
+
+
+/*    */
+
+let width = "1000px"
+width = 1000
+
+
+
+
+
+
