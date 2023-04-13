afa526318992094b5a643b122b0d39de30dbc761
Sagar <dev.sagartmg@gmail.com>
2023-04-13 18:14:34 +0545

    rest and spread and todos DOM

--- rest-spread.js
+++ rest-spread.js
@@ -39,24 +39,39 @@ let person1 = {
 */
 
 let person2 = person1  // doesnot copy the value of person 1, rather points to the location of person1 
-person2.country = "Nepal"
+// person2.country = "Nepal"
+
+
+let person3 = { ...person1 }  // ... spread operator
+person3.country = "china"
 
 /* 
     now all the values of person1 is copied to person 2
 */
 
 
-console.log("person2", person2);
+// console.log("person2", person2);
 console.log("person1", person1);
+console.log("person3", person3);
 
-/* 
-    TODO: 
+/*
+    TODO:
     modify person2's country to Nepal
-    and check what happens to person1's country if there seems to be a problem fix it. // HINT spread operator. 
+    and check what happens to person1's country if there seems to be a problem fix it. // HINT spread operator.
 */
 
 // console.log("person2-country", person2.country);
 // console.log("person1-country", person1.country);
+
+
+
+const sum = (num1, num2, num3, ...rest) => {
+    console.log({ rest })
+    return num1 + num2 + num3
+}
+
+console.log(sum(1, 2, 3, 4, 5, 6, 7));
+
 
 
 
